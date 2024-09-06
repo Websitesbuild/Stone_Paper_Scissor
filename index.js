ba67c2b1e3d0d8ba87498d7cd98e4d1e61669c81
Amit Sharma <142993534+Websitesbuild@users.noreply.github.com>
2024-09-07 01:01:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,78 @@
+const option = document.querySelectorAll('button');
+const img = document.querySelectorAll('img')
+const computer = document.querySelector('.computer')
+const player = document.querySelector('.player')
+const winText =  document.querySelector('.winner')
+const choice = ['paper','rock','scissor']
+
+let computerPoint = 0;
+let playerPoint = 0;
+
+updatePoints()
+option.forEach(element => {
+    
+    element.addEventListener('click',(e)=>{
+        let btnName = e.target.innerHTML.toLowerCase();
+        if(btnName == 'rock' || btnName == 'paper' || btnName == 'scissor'){
+            winText.innerHTML = ''
+            const playerChoice = btnName
+            const computerChoice = choice[Math.floor(Math.random()*3)]
+            img[0].classList.add('rotate')
+            img[1].classList.add('rotate')
+
+            setTimeout(()=>{
+                img[0].classList.remove('rotate')
+                img[1].classList.remove('rotate')
+                img[0].setAttribute('src',`./Img/${computerChoice}Computer.png`)
+                img[1].setAttribute('src',`./Img/${btnName}Player.png`);
+                checkWin(computerChoice,playerChoice)
+            },1000)
+
+        }
+        else if(btnName == 'reset'){
+            img[0].setAttribute('src',`./Img/rockComputer.png`)
+            img[1].setAttribute('src',`./Img/rockPlayer.png`);
+            computerPoint = 0;
+            playerPoint = 0;
+            computer.innerHTML = `Computer Points: ${computerPoint}`
+            player.innerHTML = `Player Points: ${playerPoint}`
+            winText.innerHTML = ''
+        }
+        else if(btnName == 'quit'){
+            alert("Do you really want to Quit?");
+            window.close()
+        }
+
+    })
+});
+
+function updatePoints(){
+    
+    computer.innerHTML = `Computer Points: ${computerPoint}`
+    player.innerHTML = `Your Points: ${playerPoint}`
+}
+
+function checkWin(computerChoice,playerChoice){
+    let winner;
+    if(computerChoice == playerChoice){
+        winText.innerHTML = 'Draw !!'
+    }
+    else{
+        
+        if(computerChoice == 'rock' && playerChoice == 'paper' || computerChoice == 'paper' && playerChoice == 'rock' ){
+            computerChoice == 'paper'?computerPoint++:playerPoint++
+            winner = computerChoice=='paper'?'Computer':'You'
+        }
+        else if(computerChoice == 'scissor' && playerChoice == 'rock' || computerChoice == 'rock' && playerChoice == 'scissor'){
+            computerChoice == 'rock'?computerPoint++:playerPoint++
+            winner = computerChoice=='rock'?'Computer':'You'
+        }
+        else if(computerChoice == 'paper' && playerChoice == 'scissor' || computerChoice == 'scissor' && playerChoice == 'paper'){
+            computerChoice == 'scissor'?computerPoint++:playerPoint++
+            winner = computerChoice=='scissor'?'Computer':'You'
+        }
+
+        winText.innerHTML = `${winner} Win!!`
+    }
+    updatePoints()
+}
